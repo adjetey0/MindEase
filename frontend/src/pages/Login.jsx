ec4f6f9d1039b83fb44b0493a42b68d63ee2ab01
adjetey0 <170678182+adjetey0@users.noreply.github.com>
2026-08-04 02:14:38 +0000

    made changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     setLoading(true);
     setError('');
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('http://192.168.0.103:5000/api/auth/login', {
         email,
         password,
         remember_me: rememberMe
